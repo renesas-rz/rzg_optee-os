4857ea68ace0a3529da4f793c71cd2f7b99e9dd9
takunoriotsuka <takunori.otsuka.ur@renesas.com>
2021-08-19 15:50:06 +0900

    Initial support for Renesas RZ/G2L group SoC's

---
+++ core/arch/arm/plat-rz/conf.mk
@@ -0,0 +1,20 @@
+PLATFORM_FLAVOR ?= g2l_smarc_2
+
+ifeq ($(PLATFORM_FLAVOR),g2l_dev13_1)
+$(call force,CFG_PLATFORM_GROUP_g2l,y)
+include core/arch/arm/plat-rz/g2l/rzg2l_conf.mk
+else ifeq ($(PLATFORM_FLAVOR),g2l_dev15_4)
+$(call force,CFG_PLATFORM_GROUP_g2l,y)
+include core/arch/arm/plat-rz/g2l/rzg2l_conf.mk
+else ifeq ($(PLATFORM_FLAVOR),g2l_dev21_4)
+$(call force,CFG_PLATFORM_GROUP_g2l,y)
+include core/arch/arm/plat-rz/g2l/rzg2l_conf.mk
+else ifeq ($(PLATFORM_FLAVOR),g2l_smarc_2)
+$(call force,CFG_PLATFORM_GROUP_g2l,y)
+include core/arch/arm/plat-rz/g2l/rzg2l_conf.mk
+else ifeq ($(PLATFORM_FLAVOR),g2l_smarc_4)
+$(call force,CFG_PLATFORM_GROUP_g2l,y)
+include core/arch/arm/plat-rz/g2l/rzg2l_conf.mk
+else
+$(error Unsupported PLATFORM_FLAVOR "$(PLATFORM_FLAVOR)")
+endif
