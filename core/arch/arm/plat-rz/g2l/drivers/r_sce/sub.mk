bef2de4c7102689f293cfafe2ce87f27560af622
Takunori Otsuka <takunori.otsuka.ur@renesas.com>
2022-02-15 12:00:49 +0900

    core: plat-rz: Rename the library linked to the SCE driver.

--- core/arch/arm/plat-rz/g2l/drivers/r_sce/sub.mk
+++ core/arch/arm/plat-rz/g2l/drivers/r_sce/sub.mk
@@ -1,4 +1,4 @@
 srcs-y += r_sce.c
 
 core-platform-cflags += -I$(CFG_RZ_SCE_LIB_DIR)/include
-ldflags-external += $(CFG_RZ_SCE_LIB_DIR)/libr_secure_ip_lib_g2l.a
+ldflags-external += $(CFG_RZ_SCE_LIB_DIR)/libr_secure_ip_1_0.a.1.0.0
