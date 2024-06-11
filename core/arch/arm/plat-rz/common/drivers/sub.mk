f064ad9c1eda594464d57376fbbd77ca9095461c
kaiki osakaki <kaiki.osakaki.rh@bp.renesas.com>
2024-06-11 17:37:18 +0900

    plat-rz: Fixed sub.mk for RZ/V2H.
    Signed-off-by: kaiki osakaki <kaiki.osakaki.rh@bp.renesas.com>

--- core/arch/arm/plat-rz/common/drivers/sub.mk
+++ core/arch/arm/plat-rz/common/drivers/sub.mk
@@ -7,6 +7,6 @@ subdirs-$(CFG_RZ_XSPI) += xspi
 srcs-$(CFG_RZ_SCE) += sce.c hw_rng.c
 subdirs-$(CFG_RZ_SCE) += r_sce
 
-ifeq ($(PLATFORM_FLAVOR),v2h_evk_1)
+ifeq ($(CFG_PLATFORM_GROUP_v2h),y)
 ldflags-external += $(CFG_RZ_SCE_LIB_DIR)/libr_secure_ip_2_0.a.1.0.0
 endif
