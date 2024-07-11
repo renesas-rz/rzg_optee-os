88e9051a5ea42ad8a6c1c204583f4cb30da9fb9f
kousuke.suzuki <kousuke.suzuki.te@bp.renesas.com>
2024-07-11 11:27:04 +0900

    plat-rz: Fixed TSIP driver sub.mk
    Signed-off-by: kousuke.suzuki <kousuke.suzuki.te@bp.renesas.com>

--- core/arch/arm/plat-rz/common/drivers/sub.mk
+++ core/arch/arm/plat-rz/common/drivers/sub.mk
@@ -11,7 +11,3 @@ subdirs-$(CFG_RZ_TSIP) += r_sce
 
 srcs-$(CFG_RZ_RSIP) += rsip.c hw_rng_rsip.c
 subdirs-$(CFG_RZ_RSIP) += r_rsip
-
-ifeq ($(CFG_PLATFORM_GROUP_g2l),y)
-ldflags-external += $(CFG_RZ_SCE_LIB_DIR)/libr_secure_ip_1_0.a.1.0.0
-endif
