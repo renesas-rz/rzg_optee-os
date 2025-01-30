4c34ca741bfcafaf9cc764e1bd9a2b39daa4acbd
Takunori Otsuka <takunori.otsuka.ur@renesas.com>
2025-01-30 14:10:25 +0900

    Revert "plat-rz: support for new key management tool"
    This reverts commit 3c316b99368fad8bb3918de7e77f6b314238ecfd.

--- core/arch/arm/plat-rz/common/pta/rsip/pta_rsip_rsa.c
+++ core/arch/arm/plat-rz/common/pta/rsip/pta_rsip_rsa.c
@@ -16,11 +16,13 @@ static const rsip_hash_type_t hash_type = RSIP_HASH_TYPE_SHA256;
 static const rsip_mgf_type_t mgf_type = RSIP_MGF_TYPE_MGF1_SHA256;
 static const int32_t salt_length = RSIP_RSA_SALT_LENGTH_HASH;
 
-static TEE_Result generate_hash(uint8_t const *const message, uint32_t const message_length, uint8_t *digest)
+static TEE_Result generate_hash(uint8_t const *const message, uint32_t const message_length,
+    rsip_hash_type_t *hash_type, uint8_t *digest)
 {
     fsp_err_t err;
+    *hash_type = RSIP_HASH_TYPE_SHA256;
 
-    err = R_RSIP_SHA_Compute(&rsip_instance_ctrl, hash_type, message, message_length, digest);
+    err = R_RSIP_SHA_Compute(&rsip_instance_ctrl, *hash_type, message, message_length, digest);
     switch ((uint32_t)err)
     {
         case FSP_SUCCESS:
@@ -52,6 +54,7 @@ static TEE_Result rsassa_pkcs1_v1_5_sign(uint32_t types, TEE_Param params[TEE_NU
     uint32_t message_length;
     uint8_t *signature;
     uint8_t digest[32];
+    rsip_hash_type_t hash_type;
     rsip_wrapped_key_t * wrapped_key;
 
     if (types != TEE_PARAM_TYPES(TEE_PARAM_TYPE_MEMREF_INPUT,
@@ -80,7 +83,7 @@ static TEE_Result rsassa_pkcs1_v1_5_sign(uint32_t types, TEE_Param params[TEE_NU
         return TEE_ERROR_BAD_PARAMETERS;
     }
 
-    result = generate_hash(message, message_length, digest);
+    result = generate_hash(message, message_length, &hash_type, digest);
     if (TEE_SUCCESS != result) {
         return result;
     }
@@ -130,6 +133,7 @@ static TEE_Result rsassa_pkcs1_v1_5_verify(uint32_t types, TEE_Param params[TEE_
     uint32_t message_length;
     uint8_t *signature;
     uint8_t digest[32];
+    rsip_hash_type_t hash_type;
     rsip_wrapped_key_t * wrapped_key;
 
     if (types != TEE_PARAM_TYPES(TEE_PARAM_TYPE_MEMREF_INPUT,
@@ -158,7 +162,7 @@ static TEE_Result rsassa_pkcs1_v1_5_verify(uint32_t types, TEE_Param params[TEE_
         return TEE_ERROR_BAD_PARAMETERS;
     }
 
-    result = generate_hash(message, message_length, digest);
+    result = generate_hash(message, message_length, &hash_type, digest);
     if (TEE_SUCCESS != result) {
         return result;
     }
@@ -205,6 +209,7 @@ static TEE_Result rsassa_pss_sign(uint32_t types, TEE_Param params[TEE_NUM_PARAM
     uint32_t message_length;
     uint8_t *signature;
     uint8_t digest[32];
+    rsip_hash_type_t hash_type;
     rsip_wrapped_key_t * wrapped_key;
 
     if (types != TEE_PARAM_TYPES(TEE_PARAM_TYPE_MEMREF_INPUT,
@@ -233,7 +238,7 @@ static TEE_Result rsassa_pss_sign(uint32_t types, TEE_Param params[TEE_NUM_PARAM
         return TEE_ERROR_BAD_PARAMETERS;
     }
 
-    result = generate_hash(message, message_length, digest);
+    result = generate_hash(message, message_length, &hash_type, digest);
     if (TEE_SUCCESS != result) {
         return result;
     }
@@ -283,6 +288,7 @@ static TEE_Result rsassa_pss_verify(uint32_t types, TEE_Param params[TEE_NUM_PAR
     uint8_t *message;
     uint32_t message_length;
     uint8_t digest[32];
+    rsip_hash_type_t hash_type;
     rsip_wrapped_key_t * wrapped_key;
 
     if (types != TEE_PARAM_TYPES(TEE_PARAM_TYPE_MEMREF_INPUT,
@@ -311,7 +317,7 @@ static TEE_Result rsassa_pss_verify(uint32_t types, TEE_Param params[TEE_NUM_PAR
         return TEE_ERROR_BAD_PARAMETERS;
     }
 
-    result = generate_hash(message, message_length, digest);
+    result = generate_hash(message, message_length, &hash_type, digest);
     if (TEE_SUCCESS != result) {
         return result;
     }
@@ -350,7 +356,7 @@ static TEE_Result rsassa_pss_verify(uint32_t types, TEE_Param params[TEE_NUM_PAR
 }
 
 
-static TEE_Result rsa_encrypt(uint32_t types, TEE_Param params[TEE_NUM_PARAMS],
+static TEE_Result rsa_encrypt(uint32_t types, TEE_Param params[TEE_NUM_PARAMS], 
     uint32_t cipher_size, rsip_byte_size_wrapped_key_t key_size)
 {
     fsp_err_t err;
@@ -554,7 +560,7 @@ static TEE_Result rsaes_pkcs1_v1_5_encrypt(uint32_t types, TEE_Param params[TEE_
 }
 
 
-static TEE_Result rsaes_pkcs1_v1_5_decrypt(uint32_t types, TEE_Param params[TEE_NUM_PARAMS],
+static TEE_Result rsaes_pkcs1_v1_5_decrypt(uint32_t types, TEE_Param params[TEE_NUM_PARAMS], 
     rsip_byte_size_wrapped_key_t key_size)
 {
     fsp_err_t err;
@@ -578,7 +584,7 @@ static TEE_Result rsaes_pkcs1_v1_5_decrypt(uint32_t types, TEE_Param params[TEE_
     if (!IS_ALIGNED_WITH_TYPE(params[0].memref.buffer, uint32_t)) {
         return TEE_ERROR_BAD_PARAMETERS;
     }
-
+    
     plain = (uint8_t *)params[1].memref.buffer;
     plain_length = (uint32_t)params[1].memref.size;
     if ((!IS_ALIGNED_WITH_TYPE(params[1].memref.buffer, uint32_t)) || (plain_length < cipher_length)) {
