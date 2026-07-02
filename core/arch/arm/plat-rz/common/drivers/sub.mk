61d9bd1ccf4466d0fbf0996cff234338af7b0b09
Takunori Otsuka <takunori.otsuka.ur@renesas.com>
2026-07-02 02:21:47 +0000

    core: arm: plat-rz: rename r_rsip and r_sce driver directories
    Rename the RSIP and SCE driver directories to rsip and sce,
    respectively, and update related source paths..
    
    No functional changes intended.

--- core/arch/arm/plat-rz/common/drivers/sub.mk
+++ core/arch/arm/plat-rz/common/drivers/sub.mk
@@ -1,7 +1,7 @@
 subdirs-$(CFG_RZ_SPI_MULTI) += spi_multi
 subdirs-$(CFG_RZ_XSPI) += xspi
 subdirs-$(CFG_RZ_OTP_PUCCINI) += otp/puccini
-subdirs-$(CFG_RZ_TSIP) += r_sce
-subdirs-$(CFG_RZ_RSIP) += r_rsip
+subdirs-$(CFG_RZ_TSIP) += sce
+subdirs-$(CFG_RZ_RSIP) += rsip
 subdirs-$(CFG_RZ_MBXSEM) += mbxsem
 subdirs-y += uart
